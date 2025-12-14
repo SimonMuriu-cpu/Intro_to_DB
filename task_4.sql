1ecca7ec05c2ed66a6e399786a1fc6c92a9f361a
Simon Muriu <muriusimon6@gmail.com>
2025-12-14 12:47:13 +0300

    Adding task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Switch to the database
+USE alx_book_store;
+
+-- List full description of the table 'Books'
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
